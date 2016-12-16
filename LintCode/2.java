9a2bd2b9a9a2383d05b1adc79cf189ab9e0e623d
hchpower <990652791@qq.com>
2016-12-16 11:10:24 +0800

    Success
    Accepted

--- LintCode/2.java
+++ LintCode/2.java
@@ -3,7 +3,7 @@ class Solution {
      * param n: As desciption
      * return: An integer, denote the number of trailing zeros in n!
      */
-	public long five_ten(long n)
+/*	public long five_ten(long n)
 	{
 			long a = n%5;
 			int count = 0;
@@ -12,22 +12,17 @@ class Solution {
 				count++;
 				count += five_ten(n/5);
 			}
-			else if(a == 5)
-			{
-				count++;
-				count += five_ten(n/5);
-			}
 			return count;
-	}
+	}   */
     public long trailingZeros(long n) 
 	{
         // write your code here
-		int count = 0;
-		long i = 5;
-		while(i <= n)
+		long count = 0;
+		n = n / 5;
+		while(n != 0)
 		{	
-			count += five_ten(i);
-			i += 5;
+			count += n;
+			n = n/5;
 		}
 		return count;
     }
